7a1b7ab26cda372e06e4fffd6402a83ed4fa9147
dahua <guzhaoer@gmail.com>
2019-12-05 19:06:32 +0800

    fix

--- no1jks/routers/router.go
+++ no1jks/routers/router.go
@@ -11,12 +11,30 @@ func init() {
 
 	beego.Router("/", &controllers.HomepageController{})
 	beego.Router("/news", &controllers.NewsHomeController{})
+	beego.Router("/news/:id", &controllers.NewsHomeController{})
+	beego.Router("/news/like/:id", &controllers.NewsHomeController{})
+	beego.Router("/news/comm/:id", &controllers.NewsHomeController{})
+
 	beego.Router("/question", &controllers.QuestionHomeController{})
+	beego.Router("/question/:id", &controllers.QuestionHomeController{})
+	beego.Router("/answer/:id", &controllers.QuestionHomeController{})
+	beego.Router("/answer/like/:id", &controllers.QuestionHomeController{})
+	beego.Router("/answer/comm/:id", &controllers.QuestionHomeController{})
+
 	beego.Router("/examination", &controllers.ExaminationHomeController{})
+	beego.Router("/examination/download/:id", &controllers.ExaminationHomeController{})
+
 	beego.Router("/material", &controllers.GoodHomeController{})
+	beego.Router("/material/:id", &controllers.GoodHomeController{})
+	beego.Router("/material/download/:id", &controllers.GoodHomeController{})
+
 	beego.Router("/train", &controllers.TrainHomeController{})
 
-	beego.Router("/user/login", &controllers.UserLoginController{})
-	beego.Router("/user/signup", &controllers.UserSignupController{})
 	beego.Router("/user/terms", &controllers.UserTermController{})
+	beego.Router("/user/signup", &controllers.UserSignupController{})
+	beego.Router("/user/login", &controllers.UserLoginController{})
+	beego.Router("/user/logout", &controllers.UserLoginController{})
+	beego.Router("/user/home/:id", &controllers.UserLoginController{})
+	beego.Router("/user/set/:id", &controllers.UserLoginController{})
+
 }
